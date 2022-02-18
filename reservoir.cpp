9f4ff05854c242f8cfd7588cfc8129421c887f8a
Alexander C <83742533+Alexander-col@users.noreply.github.com>
2022-02-18 06:06:49 +0000

    Task B is fully complete!

--- reservoir.cpp
+++ reservoir.cpp
@@ -44,14 +44,89 @@ float get_east_storage(std::string fetch)
 
 double get_min_east()
 {
-  
+              std::ifstream fin("Current_Reservoir_Levels.tsv");
+
+    if (fin.fail()) {
+
+        std::cerr << "File cannot be opened for reading." << std::endl;
+        exit(1); // exit if failed to open the file
+    }
+    std::string junk;        // new string variable
+    getline(fin, junk); // read one line from the file 
+
+    //Declare your variables, use proper data type    
+    std::string date;
+    double eastSt;
+    double eastEl;
+    double westSt;
+    double westEl;
+    double min;
+    min = 100;
+    //fin >> will score data within your variables
+    //order them due to its respected data tyoe
+    while(fin >> date >> eastSt >> eastEl >> westSt >> westEl) { 
+
+        // this loop reads the file line-by-line
+
+        // extracting 5 values on each iteration 
+        fin.ignore(INT_MAX, '\n'); //skips to the end of line, 
+        //ignorring the remaining columns 
+        // for example, to print the date and East basin storage:
+        
+        if (min > eastSt)
+        {
+          min = eastSt;
+          // std::cout << min << std::endl;
+        }
+        // std::cout << date << " " << eastSt << std::endl;
+    }
+  std::cout << "the min storage is: " << min << std::endl;
+
 }
 
 double get_max_east()
 {
-  
+                std::ifstream fin("Current_Reservoir_Levels.tsv");
+
+    if (fin.fail()) {
+
+        std::cerr << "File cannot be opened for reading." << std::endl;
+        exit(1); // exit if failed to open the file
+    }
+    std::string junk;        // new string variable
+    getline(fin, junk); // read one line from the file 
+
+    //Declare your variables, use proper data type    
+    std::string date;
+    double eastSt;
+    double eastEl;
+    double westSt;
+    double westEl;
+    double max;
+    max = 5;
+    //fin >> will score data within your variables
+    //order them due to its respected data tyoe
+    while(fin >> date >> eastSt >> eastEl >> westSt >> westEl) { 
+
+        // this loop reads the file line-by-line
+
+        // extracting 5 values on each iteration 
+        fin.ignore(INT_MAX, '\n'); //skips to the end of line, 
+        //ignorring the remaining columns 
+        // for example, to print the date and East basin storage:
+        
+        if (max < eastSt)
+        {
+          max = eastSt;
+        }
+        // std::cout << date << " " << eastSt << std::endl;
+    }
+  std::cout << "The max storage is " << max << std::endl;
+
 }
 int main()
 {
     std::cout << get_east_storage("01/10/2018") << std::endl;
+    std::cout << get_min_east() << std::endl;
+    std::cout << get_max_east() << std::endl;
 }
