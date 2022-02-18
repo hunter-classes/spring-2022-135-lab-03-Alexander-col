fd2137df3069e9497c1ca7f2b922b194029d8523
Alexander C <83742533+Alexander-col@users.noreply.github.com>
2022-02-18 06:28:22 +0000

    task c started!

--- reservoir.cpp
+++ reservoir.cpp
@@ -80,8 +80,7 @@ double get_min_east()
         }
         // std::cout << date << " " << eastSt << std::endl;
     }
-  std::cout << "the min storage is: " << min << std::endl;
-
+  return min;
 }
 
 double get_max_east()
@@ -121,12 +120,51 @@ double get_max_east()
         }
         // std::cout << date << " " << eastSt << std::endl;
     }
-  std::cout << "The max storage is " << max << std::endl;
+  return max;
+}
+
+std::string compare_basins(std::string fetch)
+{
+              std::ifstream fin("Current_Reservoir_Levels.tsv");
+
+    if (fin.fail()) {
+
+        std::cerr << "File cannot be opened for reading." << std::endl;
+        exit(1); // exit if failed to open the file
+    }
+    std::string junk;        // new string variable
+    getline(fin, junk); // read one line from the file 
+
+    //Declare your variables, use proper data type    
+    std::string date;
+    double eastSt;
+    double eastEl;
+    double westSt;
+    double westEl;
+    //fin >> will score data within your variables
+    //order them due to its respected data tyoe
+    while(fin >> date >> eastSt >> eastEl >> westSt >> westEl) { 
+
+        // this loop reads the file line-by-line
+
+        // extracting 5 values on each iteration 
+        fin.ignore(INT_MAX, '\n'); //skips to the end of line, 
+        //ignorring the remaining columns 
+        // for example, to print the date and East basin storage:
+        
+        if (fetch == date)
+        {
+            return eastSt;
+        }
+        // std::cout << date << " " << eastSt << std::endl;
+    }
 
 }
+
 int main()
 {
     std::cout << get_east_storage("01/10/2018") << std::endl;
     std::cout << get_min_east() << std::endl;
     std::cout << get_max_east() << std::endl;
+    std::cout << compare_basins("01/10/2018") << std::endl;
 }
